06a3aca022af32b42fd1b8acf019cdda7e2e4cc7
MatejBursik <r0929146@student.thomasmore.be>
2023-04-21 10:22:29 +0200

    cleaning

--- AdventOfCode/2021/11/test.py
+++
@@ -1,7 +0,0 @@
-a = 0
-Pen = False
-while not Pen:
-    a += 1
-    print(a)
-    if a == 4:
-        Pen = True
--- AdventOfCode/2021/17/test.py
+++
@@ -1,6 +0,0 @@
-test = []
-for i in range(10):
-    test.append([i,2])
-
-for x,y in test:
-    print(x,y)
